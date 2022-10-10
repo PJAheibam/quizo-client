6766005b0933f8d011bcc222af97639e9e5c86fd
PJAheibam <prosenjitsingha28@gmail.com>
2022-10-10 22:43:47 +0600

    theme color scheme set

---
+++ src/styles/Theme.js
@@ -0,0 +1,87 @@
+export const dark = {
+  background: "hsl(216, 18%, 16%)",
+  paper: {
+    light: "	hsl(217, 10%, 30%)",
+    main: "	hsl(217, 10%, 25%)",
+    dark: "	hsl(217, 10%, 20%)",
+  },
+  palette: {
+    primary: {
+      bg: "hsla(116, 14%, 38%, 0.25)",
+      hovered: "hsla(116, 14%, 38%, 0.5)",
+      active: "hsl(116, 14%, 38%)",
+      disabled: "hsla(116, 14%, 38%, 0.1)",
+    },
+    onPrimary: {
+      hovered: "rgba(255,255,255,0.65)",
+      unHovered: "rgba(255,255,255,0.45)",
+      active: "rgba(255,255,255,0.7)",
+      disabled: "rgba(255,255,255,0.1)",
+    },
+    secondary: {
+      active: "hsl(15, 67%, 52%)",
+      bg: "hsla(15, 67%, 52%, 0.25)",
+      hovered: "hsla(15, 67%, 52%, 0.45)",
+      disabled: "hsla(15, 67%, 52%, 0.1)",
+    },
+    onSecondary: {
+      hovered: "rgba(255,255,255,0.65)",
+      unHovered: "rgba(255,255,255,0.45)",
+      active: "rgba(255,255,255,0.7)",
+      disabled: "rgba(255,255,255,0.1)",
+    },
+    error: {
+      active: "hsl(353, 84%, 52%)",
+      bg: "hsla(353, 84%, 52%, 0.25)",
+      hovered: "hsla(353, 84%, 52%, 0.45)",
+      disabled: "hsla(353, 84%, 52%, 0.1)",
+    },
+    onError: {
+      hovered: "rgba(255,255,255,0.65)",
+      unHovered: "rgba(255,255,255,0.45)",
+      active: "rgba(255,255,255,0.7)",
+      disabled: "rgba(255,255,255,0.1)",
+    },
+    warning: {
+      active: "hsla42, 100%, 50%)",
+      bg: "hsla(42, 100%, 50%, 0.25)",
+      hovered: "hsla(42, 100%, 50%, 0.45)",
+      disabled: "hsla(42, 100%, 50%, 0.1)",
+    },
+    onWarning: {
+      hovered: "rgba(255,255,255,0.65)",
+      unHovered: "rgba(255,255,255,0.45)",
+      active: "rgba(255,255,255,0.7)",
+      disabled: "rgba(255,255,255,0.1)",
+    },
+    success: {
+      active: "hsla119, 28%, 61%)",
+      bg: "hsla(119, 28%, 61%, 0.25)",
+      hovered: "hsla(119, 28%, 61%, 0.45)",
+      disabled: "hsla(119, 28%, 61%, 0.1)",
+    },
+    onSuccess: {
+      hovered: "rgba(255,255,255,0.65)",
+      unHovered: "rgba(255,255,255,0.45)",
+      active: "rgba(255,255,255,0.7)",
+      disabled: "rgba(255,255,255,0.1)",
+    },
+    info: {
+      active: "hsl(141, 18%, 64%)",
+      bg: "hsla(141, 18%, 64%, 0.25)",
+      hovered: "hsla(141, 18%, 64%, 0.45)",
+      disabled: "hsla(141, 18%, 64%, 0.1)",
+    },
+    onInfo: {
+      hovered: "rgba(255,255,255,0.65)",
+      unHovered: "rgba(255,255,255,0.45)",
+      active: "rgba(255,255,255,0.7)",
+      disabled: "rgba(255,255,255,0.1)",
+    },
+  },
+  text: {
+    primary: "hsla(0, 0%, 93%, 0.87)",
+    secondary: "hsla(0, 0%, 93%, 0.75)",
+    disabled: "hsla(0, 0%, 93%, 0.4)",
+  },
+};
