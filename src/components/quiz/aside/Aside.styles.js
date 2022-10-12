0ccb4fbbfa2e3c30f097e0c4a53244123cc5e161
PJAheibam <prosenjitsingha28@gmail.com>
2022-10-12 20:47:19 +0600

    warning removed

--- src/components/quiz/aside/Aside.styles.js
+++ src/components/quiz/aside/Aside.styles.js
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { device } from "../../../utils/device";
 
 export const AsideWrapper = styled.article`
